b00279e16779a456c16b11d2bea2e68dea214691
Rory How <me@rory.how>
2021-02-27 00:00:48 +0200

    Tidy up and format code a little bit

--- registerStravaWebhook/main.go
+++ registerStravaWebhook/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -14,12 +13,10 @@ type ValidationResponse struct {
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
 	mode := request.QueryStringParameters["hub.mode"]
 	challenge := request.QueryStringParameters["hub.challenge"]
 	verifyToken := request.QueryStringParameters["hub.verify_token"]
 
-	// TODO update verifyToken to be environment variable
 	if mode == "subscribe" && verifyToken == os.Getenv("STRAVA_WEBHOOK_VERIFY_TOKEN") {
 		validationResponse := ValidationResponse{
 			ChallengeResponse: challenge,
@@ -34,8 +31,11 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 			"Content-Type": "application/json",
 		}
 
-		log.Printf("Registering handler, returning response:\n" + string(response))
-		return events.APIGatewayProxyResponse{Body: string(response), Headers: responseHeaders, StatusCode: 200}, nil
+		return events.APIGatewayProxyResponse{
+			Body:       string(response),
+			Headers:    responseHeaders,
+			StatusCode: 200,
+		}, nil
 	}
 
 	return events.APIGatewayProxyResponse{Body: "Invalid Request", StatusCode: 400}, nil
