bf23a9c1c8b1f3f65a6b1759d3946492d05645d2
Rory How <me@rory.how>
2021-05-23 17:37:53 +0300

    Fix dynamodb schema typo

--- db/slack_user.go
+++ db/slack_user.go
@@ -3,7 +3,7 @@ package db
 type SlackUser struct {
 	UserID       string `json:"user_id" dynamodbav:"UserID"`
 	UserName     string `json:"user_name" dynamodbav:"UserName"`
-	ChannelID    string `json:"channel_id" dyanmodbav:"ChannelID"`
+	ChannelID    string `json:"channel_id" dynamodbav:"ChannelID"`
 	TeamID       string `json:"team_id" dynamodbav:"TeamID"`
 	EnterpriseID string `json:"enterprise_id" dynamodbav:"EnterpriseID"`
 }
